41667243579dfa8f9358140caeef6b0b1194cfdc
Sebastian Green-Husted <ricecake@tfm.nu>
2015-03-31 16:27:27 -0400

    skeleton parser/serializer

--- src/gen_udt_proto.erl
+++ src/gen_udt_proto.erl
@@ -4,3 +4,7 @@
          parse/2, serialize/2 ]).
          
 parse(Packet) -> parse(Packet, []).
+serialize(Packet) -> serialize(Packet, []).
+
+parse(Packet, Opts) when is_binary(Packet) -> {ok, #{}}.
+serialize(Packet, Opts) when is_map(Packet) -> {ok, <<>>}.
