6b86a2fcc9a15ebe20a9d981b80c80f3210eba60
Sebastian Green-Husted <ricecake@tfm.nu>
2015-04-02 18:25:56 -0400

    start of documentation for protocol

--- src/gen_udt_proto.erl
+++ src/gen_udt_proto.erl
@@ -6,5 +6,21 @@
 parse(Packet) -> parse(Packet, []).
 serialize(Packet) -> serialize(Packet, []).
 
+
+%   The data packet header structure is as following.
+%
+%   0                   1                   2                   3
+%   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+%   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+%   |0|                     Packet Sequence Number                  |
+%   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+%   |FF |O|                     Message Number                      |
+%   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+%   |                          Time Stamp                           |
+%   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+%   |                    Destination Socket ID                      |
+%   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 parse(Packet, Opts) when is_binary(Packet) -> {ok, #{}}.
+
+
 serialize(Packet, Opts) when is_map(Packet) -> {ok, <<"">>}.
